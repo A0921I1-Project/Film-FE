17cb90678d5fb4d6a4a2d293165ae26b2c8fd22a
huynh <lqh.nuchaku@gmail.com>
2022-08-20 19:58:44 +0700

    update

--- cinema/src/app/app.module.ts
+++ cinema/src/app/app.module.ts
@@ -4,16 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
+import {SecurityRoutingModule} from './security/security-routing.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SecurityModule} from './security/security.module';
+import {AuthenticationService} from './security/service/authentication.service';
+import {TokenStorageService} from './security/service/token-storage-service.service';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SecurityModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AuthenticationService,
+    TokenStorageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

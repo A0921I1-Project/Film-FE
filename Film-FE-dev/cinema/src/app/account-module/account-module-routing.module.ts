56d69bc64e0b68bcc9457f6092f2d80e9454c0b5
Trần Văn Sơn <tranvanson231099@gmail.com>
2022-08-25 23:27:55 +0700

    update

---
+++ Film-FE-dev/cinema/src/app/account-module/account-module-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {EmployeeListComponent} from './employee/employee-list/employee-list.component';
+
+const routes: Routes = [
+  {
+    path : 'employee',
+    component: EmployeeListComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AccountModuleRoutingModule { }
